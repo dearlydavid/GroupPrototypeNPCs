9b429a40f31d0455d7409bb12a0779c1a15b8415
Bonnie Bolton <bonnie.b.junk@gmail.com>
2019-04-03 13:31:45 -0500

    added some scripts

---
+++ NPCs Prototype/Assets/Scripts/npc.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class npc : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    private NavMeshAgent myAgent;
+    private int currentWaypoint;
+    private bool talking;
+    public GameObject QuestText;
+    private bool questItemFound;
+    public QuestItem questItem;
+    public bool questActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        myAgent.destination = waypoints[currentWaypoint].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!questItemFound && questItem.itemCollected)
+        {
+            questItemFound = true;
+            Destroy(questItem.gameObject);
+        }
+        if (Vector3.Distance(myAgent.destination, transform.position) <= 1 && !talking)
+        {
+            currentWaypoint++;
+            if(currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+            myAgent.destination = waypoints[currentWaypoint].transform.position;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            myAgent.destination = transform.position;
+            if (!talking)
+            {
+                if (questItemFound)
+                {
+                    QuestText.GetComponent<Text>().text = "Thanks, thats just what I wanted!";
+                }
+                questActive = true;
+                GetComponentInChildren<Animator>().SetTrigger("Talk");
+                talking = true;
+                QuestText.SetActive(true);
+            }
+            transform.LookAt(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (talking)
+        {
+            GetComponentInChildren<Animator>().SetTrigger("StopTalking");
+            talking = false;
+            QuestText.SetActive(false);
+        }
+    }
+
+
+}
---
+++ NPCs Prototype/Assets/Scripts/player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float distanceToGround;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, distanceToGround + .05f);
+
+        //if (Input.GetKey(KeyCode.Space) && isGrounded)
+        //{
+        //    GetComponent<Rigidbody>().AddForce(Vector3.up*200);
+        //}
+
+        float rotation = Input.GetAxis("Horizontal") * 240;
+        float translation = Input.GetAxis("Vertical") * 5;
+        rotation *= Time.deltaTime;
+        translation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+        transform.Translate(0, 0, translation);
+
+        if (isGrounded) { anim.SetFloat("Speed", Mathf.Abs(translation * 20)); }
+        else { anim.SetFloat("Speed", 0); }
+         
+        //GetComponent<Rigidbody>().AddForce(Vector3.forward * );
+    }
+}
